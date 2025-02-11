8eed69897d1ae15341ebdf9e718b089a9fa3a7e2
KoJa <12777785+Litas-dev@users.noreply.github.com>
2025-02-11 18:11:42 +0100

    Update memprofiling.py
    Improvements:
    1. Error Handling: Now checks if the C file exists before parsing.
    2. Interactive Input: Allows the user to input a file path or inline C code.
    3. Better Memory Profiling: Uses tracemalloc for more detailed memory analysis.
    4. Improved AST Visitor: The visitor now properly tracks and prints parent nodes.
    Cleaner Output: Organizes print statements to be more readable.

--- utils/internal/memprofiling.py
+++ utils/internal/memprofiling.py
@@ -1,121 +1,78 @@
 import sys
-from pycparser import parse_file
-from pycparser.c_ast import *
-from pycparser.c_parser import CParser, Coord, ParseError
-from pycparser.c_lexer import CLexer
+import os
+import resource
+import tracemalloc
+from pycparser import parse_file, CParser
+from pycparser.c_ast import NodeVisitor
 
+def parse_c_file(filepath):
+    """ Parse a C file and return its AST. """
+    if not os.path.exists(filepath):
+        print(f"Error: File '{filepath}' not found.")
+        return None
+    
+    try:
+        return parse_file(filepath, use_cpp=True)
+    except Exception as e:
+        print(f"Error parsing file: {e}")
+        return None
 
-def expand_decl(decl):
-    """ Converts the declaration into a nested list.
-    """
-    typ = type(decl)
-
-    if typ == TypeDecl:
-        return ['TypeDecl', expand_decl(decl.type)]
-    elif typ == IdentifierType:
-        return ['IdentifierType', decl.names]
-    elif typ == ID:
-        return ['ID', decl.name]
-    elif typ in [Struct, Union]:
-        decls = [expand_decl(d) for d in decl.decls or []]
-        return [typ.__name__, decl.name, decls]
-    else:
-        nested = expand_decl(decl.type)
-
-        if typ == Decl:
-            if decl.quals:
-                return ['Decl', decl.quals, decl.name, nested]
-            else:
-                return ['Decl', decl.name, nested]
-        elif typ == Typename: # for function parameters
-            if decl.quals:
-                return ['Typename', decl.quals, nested]
-            else:
-                return ['Typename', nested]
-        elif typ == ArrayDecl:
-            dimval = decl.dim.value if decl.dim else ''
-            return ['ArrayDecl', dimval, nested]
-        elif typ == PtrDecl:
-            return ['PtrDecl', nested]
-        elif typ == Typedef:
-            return ['Typedef', decl.name, nested]
-        elif typ == FuncDecl:
-            if decl.args:
-                params = [expand_decl(param) for param in decl.args.params]
-            else:
-                params = []
-            return ['FuncDecl', params, nested]
-
-#-----------------------------------------------------------------
-class NodeVisitor(object):
-    def __init__(self):
-        self.current_parent = None
-
-    def visit(self, node):
-        """ Visit a node.
-        """
-        method = 'visit_' + node.__class__.__name__
-        visitor = getattr(self, method, self.generic_visit)
-        return visitor(node)
-
-    def visit_FuncCall(self, node):
-        print("Visiting FuncCall")
-        print(node.show())
-        print('---- parent ----')
-        print(self.current_parent.show())
-
-    def generic_visit(self, node):
-        """ Called if no explicit visitor function exists for a
-            node. Implements preorder visiting of the node.
-        """
-        oldparent = self.current_parent
-        self.current_parent = node
-        for c in node.children():
-            self.visit(c)
-        self.current_parent = oldparent
-
-
-def heapyprofile():
-    # pip install guppy
-    # [works on python 2.7, AFAIK]
-    from guppy import hpy
-    import gc
-
-    hp = hpy()
-    ast = parse_file('/tmp/197.c')
-    gc.collect()
-    h = hp.heap()
-    print(h)
-
-
-def memprofile():
-    import resource
-    import tracemalloc
-
+def memory_profile():
+    """ Profile memory usage while parsing C code. """
     tracemalloc.start()
-
-    ast = parse_file('/tmp/197.c')
-
-    print('Memory usage: %s (kb)' %
-            resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
-
+    filepath = input("Enter C file path (or press Enter to use default '/tmp/197.c'): ") or "/tmp/197.c"
+    
+    ast = parse_c_file(filepath)
+    if ast is None:
+        return
+    
+    print('Memory usage: %s KB' % resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
     snapshot = tracemalloc.take_snapshot()
-    print("[ tracemalloc stats ]")
-    for stat in snapshot.statistics('lineno')[:20]:
+    print("[ Tracemalloc Stats ]")
+    for stat in snapshot.statistics('lineno')[:10]:
         print(stat)
 
+class CustomVisitor(NodeVisitor):
+    """ Custom AST visitor to analyze function calls. """
+    def __init__(self):
+        super().__init__()
+        self.current_parent = None
+
+    def visit_FuncCall(self, node):
+        print("\n[ Visiting Function Call ]")
+        node.show()
+        print('[ Parent Node ]')
+        if self.current_parent:
+            self.current_parent.show()
+        else:
+            print("No parent node.")
+
+    def generic_visit(self, node):
+        old_parent = self.current_parent
+        self.current_parent = node
+        for _, child in node.children():
+            self.visit(child)
+        self.current_parent = old_parent
 
 if __name__ == "__main__":
-    source_code = r'''void foo() {
-    L"hi" L"there";
-}
-    '''
-
-    memprofile()
-    #heapyprofile()
-
-    #parser = CParser()
-    #ast = parser.parse(source_code, filename='zz')
-    #ast.show(showcoord=True, attrnames=True, nodenames=True)
-
-
+    # Ask user for file input or use inline source
+    source_code = input("Enter inline C code (or press Enter to use a sample): ")
+    if not source_code:
+        source_code = """void foo() {
+            printf(\"Hello, world!\");
+        }"""
+    
+    parser = CParser()
+    try:
+        ast = parser.parse(source_code, filename='<inline>')
+        print("\n[ Parsed AST ]")
+        ast.show()
+        
+        print("\n[ Running AST Visitor ]")
+        visitor = CustomVisitor()
+        visitor.visit(ast)
+    except Exception as e:
+        print(f"Parsing error: {e}")
+    
+    # Run memory profiling
+    memory_profile()
